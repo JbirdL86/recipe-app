93ca136c201837b5196dd713c6ec529ff4d3beee
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-04 18:14:53 -0300

    feature: fix routes adding resources to users

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,4 @@
 class UserController < ApplicationController
   def create
-    @user = User.new
-
-    redirect_to recipes()
-    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  devise_scope :user do
-    authenticated :user do
-      root 'recipe#index', as: :authenticated_root
-    end
-    
-    unauthenticated do
-      root 'devise/sessions#new', as: :unauthenticated_root
-    end
-  end 
+  resources :users
 
   resources :foods, only: [:create, :index, :destroy]
 
