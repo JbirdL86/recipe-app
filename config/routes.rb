209e24e41eeff465e084277d85345eb9837be8d9
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-04 15:56:49 -0300

    feature: add entities routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :foods, only: [:create, :index, :destroy]
+
+  resources :recipes
+  
+  resources :public_recipes, only: :index
+
+  resources :general_public_list, only: :index 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "recipes#index"
 end
