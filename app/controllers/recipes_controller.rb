13755b031e0357731fbf6f95935dd2710629d708
JbirdL86 <jgonzalez.wj@gmail.com>
2022-01-06 15:43:07 -0600

    Add actions to recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,30 @@
 class RecipesController < ApplicationController
+  
   def index
     @user = current_user
+    @recipes = Recipe.all
   end
+
+  def new
+    @user = current_user
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      flash[:succes] = 'Recipe successfully created!'
+      redirect_to recipes_path() 
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :coocking_time, :description, :public, :user_id)
+  end
+
 end
