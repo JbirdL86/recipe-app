ed72c0ba89b6ace39525d7e80c558cc91b6c443a
JbirdL86 <jgonzalez.wj@gmail.com>
2022-01-06 15:44:22 -0600

    Add migrations to schema

--- db/schema.rb
+++ db/schema.rb
@@ -73,8 +73,8 @@ ActiveRecord::Schema.define(version: 2022_01_04_200353) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at", precision: 6
-    t.datetime "remember_created_at", precision: 6
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
